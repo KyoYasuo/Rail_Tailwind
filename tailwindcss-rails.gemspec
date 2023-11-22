74c241030e505ece26d7572cd145548f12dd63e7
KyoYasuo <kyoyasuo221@gmail.com>
2023-11-22 18:14:27 +0900

    initial

---
+++ tailwindcss-rails.gemspec
@@ -0,0 +1,24 @@
+require_relative "lib/tailwindcss/version"
+
+Gem::Specification.new do |spec|
+  spec.name        = "tailwindcss-rails"
+  spec.version     = Tailwindcss::VERSION
+  spec.authors     = [ "David Heinemeier Hansson" ]
+  spec.email       = "david@loudthinking.com"
+  spec.homepage    = "https://github.com/rails/tailwindcss-rails"
+  spec.summary     = "Integrate Tailwind CSS with the asset pipeline in Rails."
+  spec.license     = "MIT"
+
+  spec.metadata = {
+    "homepage_uri" => spec.homepage,
+    "rubygems_mfa_required" => "true"
+  }
+
+  spec.required_rubygems_version = ">= 3.2.0" # for Gem::Platform#match_gem?
+
+  spec.files = Dir["{app,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
+  spec.bindir = "exe"
+  spec.executables << "tailwindcss"
+
+  spec.add_dependency "railties", ">= 6.0.0"
+end
---
+++ test/integration/user_journey_test.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+# reproduce the documented user journey for installing and running tailwindcss-rails
+# this is run in the CI pipeline, non-zero exit code indicates a failure
+
+set -o pipefail
+set -eux
+
+# fetch the upstream executables
+bundle exec rake download
+
+# create a rails app in a directory with spaces in the name (#176, #184)
+rm -rf "Has A Space"
+mkdir "Has A Space"
+pushd "Has A Space"
+
+gem install rails
+rails new test-app --skip-bundle
+pushd test-app
+
+# install tailwindcss-rails
+bundle add tailwindcss-rails --path="../.."
+bundle install
+
+bin/rails tailwindcss:install
+
+cat <<EOF >> Rakefile
+task :still_here do
+  puts "Rake process did not exit early"
+end
+EOF
+
+# ensure rake tasks don't exec (#188)
+bin/rails tailwindcss:build still_here | grep "Rake process did not exit early"
